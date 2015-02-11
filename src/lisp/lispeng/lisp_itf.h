e3543350831be85098538ee50216d715ea9186b8
ufasoft <dev@ufasoft.com>
2015-02-11 19:44:10 +0500

    -

--- src/lisp/lispeng/lisp_itf.h
+++ src/lisp/lispeng/lisp_itf.h
@@ -9,7 +9,9 @@ __BEGIN_DECLS
 	HRESULT __stdcall LispLoad(LISPHANDLE h, const char *filename);
 	void __stdcall LispBreak(LISPHANDLE);
 	LISPHANDLE __stdcall LispGetCurrent();
+#ifdef WIN32
 	BSTR __stdcall LispEval(LISPHANDLE, const char *s);
+#endif
 	void __stdcall LispSetStandardStream(LISPHANDLE, int idx, void *stdStream);
 
 __END_DECLS
